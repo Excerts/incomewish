d9670b324ab9dcb2ad2048d75ff6774028fb6d65
class <class@CST_N417_19.cst.citytech.cuny.edu>
2015-02-18 17:31:04 -0500

    commiting the new jave project

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -3,20 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package incomewish;
 
 /**
  *
  * @author class
  */
-public class incomewish {
+public class JavaApplication4 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    
+        String name;
+        int age;
+        double annualpay;
+        name = "Ryan";
+        age = 20;
+        annualpay = 200000;
+        System.out.println("My name is " + name + "," + "my age is " + age + "and");
+        System.out.println("I hope to earn $" + annualpay + "per year.");
+
     }
-    
+
 }
